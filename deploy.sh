7423aa397712a1391f45a0c3292c1549b45f3c3e
Christian Zosel <mail@thgersdorf.net>
2017-05-14 13:41:40 +0200

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# git pull is not in here yet because config mgmt isnt done yet
+git stash
+git pull
+git stash apply
 (cd tt-client && npm i && npm run build)
 (cd server && npm i && npm run build && pm2 restart click-tt)
