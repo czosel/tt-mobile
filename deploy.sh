3c4444637230aef2ec148f2086125e5e5d84de51
Christian Zosel <christian@zosel.ch>
2019-12-08 20:25:40 +0100

    adapt deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ stage=$(<stage)
 
 git pull
 (cd client && npm i && npm run build)
-(cd server && npm i && npm run build && pm2 restart "click-tt-${stage}")
+(cd server && npm i && npm run build && pm2 restart "tt-mobile-${stage}")
