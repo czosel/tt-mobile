b1c54cf24a6660ac1776e521d4bc7adb69732be5
Christian Zosel <christian@zosel.ch>
2019-12-12 23:35:33 +0100

    add robots.txt

--- client/preact.config.js
+++ client/preact.config.js
@@ -1,5 +1,9 @@
 import envVars from 'preact-cli-plugin-env-vars'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
 
 export default function(config, env, helpers) {
   envVars(config, env, helpers)
+  config.plugins.push(
+    new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: `*.*` }])
+  )
 }
