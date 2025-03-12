91fd882aa557e0a106de97d8ee09c87335c77113
Christian Zosel <christian@zosel.ch>
2025-03-12 20:07:56 +0100

    chore: recreate docs app based on "create-vue"

--- widget-docs/src/App.vue
+++ widget-docs/src/App.vue
@@ -275,7 +275,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
--- widget-docs/src/main.js
+++ widget-docs/src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueHighlightJS from "vue-highlightjs";
 
+import "./assets/main.css";
+
 Vue.config.productionTip = false;
 Vue.use(VueHighlightJS);
 
