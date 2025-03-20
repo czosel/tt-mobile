cf729750102e45889b9357e73096e863cb5f083d
Christian Zosel <christian@zosel.ch>
2025-03-20 08:27:20 +0100

    feat: team schedule widget

--- widget-docs/src/components/TtTeam.vue
+++ widget-docs/src/components/TtTeam.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="table"></table>
+  <div class="table"></div>
 </template>
 
 <script>
@@ -7,13 +7,41 @@ export default {
   name: "TtTeam",
   props: {
     url: String,
+    showPlayers: {
+      type: Boolean,
+      default: true,
+    },
+    showSchedule: {
+      type: Boolean,
+      default: false,
+    },
   },
   mounted() {
-    this.url && TTmobile.team(this.url, this.$el);
+    this.url &&
+      TTmobile.team(this.url, this.$el, {
+        showPlayers: this.showPlayers,
+        showSchedule: this.showSchedule,
+      });
   },
   watch: {
     url(url) {
-      url && TTmobile.team(url, this.$el);
+      url &&
+        TTmobile.team(url, this.$el, {
+          showPlayers: this.showPlayers,
+          showSchedule: this.showSchedule,
+        });
+    },
+    showPlayers(showPlayers) {
+      TTmobile.team(this.url, this.$el, {
+        showPlayers,
+        showSchedule: this.showSchedule,
+      });
+    },
+    showSchedule(showSchedule) {
+      TTmobile.team(this.url, this.$el, {
+        showPlayers: this.showPlayers,
+        showSchedule,
+      });
     },
   },
 };
